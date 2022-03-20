ffa674c665309f72923988f88e3b508ab6715743
nc-hung <nguyenhung11a6@gmail.com>
2022-03-21 00:56:41 +0700

    update

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ $(document).ready(() => {
 
     // Thêm "<span class='close'>x</span>" vào từng thẻ li
     $('li').append("<span class='close'>x</span>")
+        // Xử lý sự kiện khi nhấn nút close
+    let a = $('.close');
+    for (let i = 0; i < a.length; i++) {
+        $(a[i]).click(function() {
+            $(this).parent().remove();
+        })
+    }
+    // console.log($("#to-do-list").children(), "list")
 
-
+    // Xử lý sự kiện click vào mỗi li
+    let lists = $('#to-do-list').children();
+    for (let i = 0; i < lists.length; i++) {
+        $(lists[i]).click(function() {
+            //toggleClass: chuyển đổi qua lại giữa thêm và xóa class
+            $(this).toggleClass('checked');
+        });
+    }
 
     // Xử lý sự kiện click vào nút [Add]
     $(".btn-add").click(function() {
@@ -18,7 +33,14 @@ $(document).ready(() => {
             let li = document.createElement("li");
             $(li).text(value);
             // Tạo nút close cho thẻ li, gắn class và thêm nút này vào li
-            $(li).append("<span class='close'>x</span>")
+            // $(li).append("<span class='close'>x</span>")
+            let close = document.createElement('span');
+            $(close).text('x');
+            $(close).addClass('close')
+            $(li).append($(close));
+            $(li).click(function() {
+                    $(this).toggleClass('checked');
+                })
                 // Thêm li mới vừa tạo vào phần tử con đầu tiên của ul
             $('#to-do-list').prepend($(li));
         }
@@ -34,31 +56,8 @@ $(document).ready(() => {
             })
         }
 
-        // Xử lý sự kiện click vào mỗi li ---------------------
-        // let lists = $('#to-do-list').children();
-        // for (let i = 0; i < lists.length; i++) {
-        //     $(lists[i]).click(function() {
-        //         //toggleClass: chuyển đổi qua lại giữa thêm và xóa class
-        //         $(lists[i]).toggleClass('checked');
-        //     });
-        // }
-    });
-    // Xử lý sự kiện khi nhấn nút close
-    let a = $('.close');
-    for (let i = 0; i < a.length; i++) {
-        $(a[i]).click(function() {
-            $(this).parent().remove();
-        })
-    }
-    // console.log($("#to-do-list").children(), "list")
 
-    // Xử lý sự kiện click vào mỗi li
-    let lists = $('#to-do-list').children();
-    for (let i = 0; i < lists.length; i++) {
-        $(lists[i]).click(function() {
-            //toggleClass: chuyển đổi qua lại giữa thêm và xóa class
-            $(lists[i]).toggleClass('checked');
-        });
-    }
+    });
+
 });
 //refactor
